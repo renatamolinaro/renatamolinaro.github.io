cb1392e36bd25bb0ee9f8eaefbc9fc1d0f7417c6
Luis Ferreira <luisrbferreira@gmail.com>
2018-07-15 03:32:10 -0300

    Mascara celular

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $.mask.definitions['~'] = "[+-]";
-    $("#celular").mask("(99) 99999-9999");
+    // $.mask.definitions['~'] = "[+-]";
+    // $("#celular").mask("(99) 99999-9999");
+
+    // $.mask.definitions['~'] = "[+-]";
+    // $("#celular-cancun").mask("(99) 99999-9999");
+
+    $('#celular').mask('(00) 00000-0000');
+    $('#celular-cancun').mask('(00) 00000-0000');
 
     $('#carousel').carousel(
         {
